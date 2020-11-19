c606b3ee880422849f650e7e3bf3debbe50fc2d9
Marcus Fredriksson <marcus@marcusfredriksson.com>
2020-11-19 15:29:19 +0100

    Replaces placeholder mac-address for better UX

--- scroll.sh
+++ scroll.sh
@@ -1,9 +1,9 @@
 /etc/init.d/bluetooth stop
 /etc/init.d/bluetooth start
 sleep 3s
-echo 'connect 00:00:00:00:00:00' | bluetoothctl
+echo 'connect DEVICE-MAC-ADDRESS' | bluetoothctl
 sleep 1s
-echo 'connect 00:00:00:00:00:01' | bluetoothctl
+echo 'connect ANOTHER-DEVICE-MAC-ADDRESS' | bluetoothctl
 sleep 5s
 sudo modprobe -r hid-magicmouse
 sudo modprobe hid-magicmouse emulate_scroll_wheel=Y emulate_3button=Y middle_click_3finger=Y scroll_acceleration=Y scroll_speed=12
